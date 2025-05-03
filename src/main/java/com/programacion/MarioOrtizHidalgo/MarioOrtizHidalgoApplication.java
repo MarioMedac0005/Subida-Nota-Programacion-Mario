a3f861bcab05ad202919543dc5c3be8316624d0c
MariOrtizHidalgo <ortizhidalgomario@gmail.com>
2025-05-03 15:05:52 +0200

    Inicio del Proyecto

---
+++ src/main/java/com/programacion/MarioOrtizHidalgo/MarioOrtizHidalgoApplication.java
@@ -0,0 +1,13 @@
+package com.programacion.MarioOrtizHidalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioOrtizHidalgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioOrtizHidalgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programacion/MarioOrtizHidalgo/MarioOrtizHidalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacion.MarioOrtizHidalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioOrtizHidalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

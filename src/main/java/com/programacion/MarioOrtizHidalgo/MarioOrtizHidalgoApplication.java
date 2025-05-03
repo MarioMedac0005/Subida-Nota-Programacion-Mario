346752d37eed31780e142fe46c9396efd7de4343
MariOrtizHidalgo <ortizhidalgomario@gmail.com>
2025-05-03 16:48:10 +0200

    Swagger UI ultimo

--- src/main/java/com/programacion/MarioOrtizHidalgo/MarioOrtizHidalgoApplication.java
+++ src/main/java/com/programacion/MarioOrtizHidalgo/MarioOrtizHidalgoApplication.java
@@ -2,7 +2,21 @@ package com.programacion.MarioOrtizHidalgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+		info = @Info(
+				description = "Documentación sobre la API Rest para poder hacer un CRUD sobre una lista de productos.",
+				version = "1.0",
+				contact = @Contact(
+						email = "ortizhidalgomario@gmail.com",
+						name = "Mario Ortiz Hidalgo"
+				),
+				title = "API Rest para la creación de una lista de productos básicos."
+		)
+)
 @SpringBootApplication
 public class MarioOrtizHidalgoApplication {
 
